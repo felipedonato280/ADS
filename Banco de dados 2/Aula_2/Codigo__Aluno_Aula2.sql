8c81d3f0508c32e56ca0bd84d22e4364201dc3c1
felipedonato280 <felipedonato280@gmail.com>
2026-04-24 16:25:00 -0300

    Exercicios DB

--- Banco de dados 2/Aula_2/Codigo__Aluno_Aula2.sql
+++ Banco de dados 2/Aula_2/Codigo__Aluno_Aula2.sql
@@ -6,24 +6,31 @@ create table aluno (
   cpf char(11),
   email varchar(200),
   indiceRendimento numeric(4,2),
-  primary key(Codaluno)) 
-  
+  primary key(Codaluno));
+
 --MOSTRANDO A TABELA ALUNO 
   select * from aluno 
 
 --POPULANDO (INSERINDO REGISTROS) NA TABELA ALUNO 
-INSERT INTO aluno (Nomaluno,Datanasc,cpf,email,indiceRendimento) VALUES ('JOAO', '1998/03/11','11111111111','joao@gmail.com','8.45');
+INSERT INTO aluno (Nomaluno,Datanasc,cpf,email,indiceRendimento) 
+VALUES 
+	('JOAO', '1998/03/11','11111111111','joao@gmail.com','8.45'),
+	('MARIA', '1997/12/31','22222222222','maria@gmail.com','6.78'),
+	('MANUEL', '1987/05/04','33333333333','manuel@gmail.com','7.98'),
+	('JOSÉ', '1985/03/30','44444444444','jose@gmail.com','5.45'),
+	('ANA', '1978/02/23','55555555555','ana@gmail.com','9.03');
 
-INSERT INTO aluno (Nomaluno,Datanasc,cpf,email,indiceRendimento) VALUES ('MARIA', '1997/12/31','22222222222','maria@gmail.com','6.78');
-
-INSERT INTO aluno (Nomaluno,Datanasc,cpf,email,indiceRendimento) VALUES ('MANUEL', '1987/05/04','33333333333','manuel@gmail.com','7.98');
-
-INSERT INTO aluno (Nomaluno,Datanasc,cpf,email,indiceRendimento) VALUES ('JOSÉ', '1985/03/30','44444444444','jose@gmail.com','5.45');
-
-INSERT INTO aluno (Nomaluno,Datanasc,cpf,email,indiceRendimento) VALUES ('ANA', '1978/02/23','55555555555','ana@gmail.com','9.03');
+INSERT INTO aluno (Nomaluno,Datanasc,cpf,email,indiceRendimento)
+VALUES
+	('Felipe', '2025-10-26', '03003003022', 'felipeteste@gmail.com', '10');
 
 --MOSTRANDO TODOS OS REGISTROS DA TABELA ALUNO 
-  select * from aluno 
+select * from aluno 
 
 --Comando para exibir a data em formato PT-BR (DD-MM-YYYY)
-SET datestyle TO postgres, DMY; 
+
+SET datestyle TO postgres, DMY;
+SELECT Datanasc FROM aluno;
+
+SET datestyle TO ISO, YMD;
+SELECT Datanasc FROM aluno;
--- Banco de dados 2/Aula_2/Consultas_Exercicios_Aula2.sql
+++ Banco de dados 2/Aula_2/Consultas_Exercicios_Aula2.sql
@@ -32,91 +32,149 @@ SELECT Nomaluno, email FROM aluno WHERE (extract(year from datanasc)>'1990');
 
 --8) Excluir os dados dos alunos com índice de rendimento menor que 6. 
 
-
+DELETE FROM aluno WHERE indiceRendimento < 6;
+SELECT * FROM aluno;
 
 --9) Listar todos os registros da tabela aluno. 
 
-
+SELECT * FROM aluno;
 
 --10) Mostrar o total de alunos da tabela aluno. 
 
-
+SELECT COUNT(*) FROM aluno;
 
 --11) Insira a coluna ‘sexo’ do tipo char (1) na tabela aluno. 
 
-
+ALTER TABLE aluno
+ADD COLUMN sexo char(1);
 
 --12) Insira o sexo 'M' ou 'F' de cada aluno da tabela aluno. 
 
+UPDATE aluno SET sexo = 'M' WHERE sexo IS NULL;
 
+UPDATE aluno SET sexo = 'F' WHERE Nomaluno LIKE '%A';
+
+SELECT * FROM aluno;
 
 --13) Mostre o total de alunos do sexo masculino. 
 
-
+SELECT COUNT(*) FROM aluno WHERE sexo = 'M';
 
 --14) Liste o nome dos alunos do sexo masculino. 
 
-
+SELECT Nomaluno FROM aluno WHERE sexo = 'M';
 
 --15) Mostre o total de alunos do sexo feminino. 
 
-
+SELECT COUNT(*) FROM aluno WHERE sexo = 'F';
 
 --16) Liste o nome dos alunos do sexo feminino. 
 
-
+SELECT Nomaluno FROM aluno WHERE sexo = 'F';
 
 --17) Mostre a soma do índice de rendimento de todos os alunos. 
 
-
+SELECT SUM(indiceRendimento) FROM aluno;
 
 --18) Mostre a soma do índice de rendimento dos alunos do sexo feminino. 
 
-
+SELECT SUM(indiceRendimento) FROM aluno WHERE sexo = 'F';
 
 --19) Mostre a soma do índice de rendimento dos alunos do sexo masculino. 
 
-
+SELECT SUM(indiceRendimento) FROM aluno WHERE sexo = 'M';
 
 --20) Mostre a média do índice de rendimento dos alunos da tabela aluno. 
 
+SELECT AVG(indiceRendimento) FROM aluno;
+SELECT indiceRendimento FROM aluno;
 
+-- Tem como calcular na mao, se quiser
+SELECT SUM(indiceRendimento) / COUNT(indiceRendimento) FROM aluno;
+
+-- Teste usando o group by
+SELECT sexo, AVG(indiceRendimento)
+FROM aluno
+GROUP BY sexo;
 
 --21) Mostre a média do índice de rendimento dos alunos do sexo feminino. 
 
-
+SELECT AVG(indiceRendimento) FROM aluno WHERE sexo = 'F';
 
 --22) Mostre a média do índice de rendimento dos alunos do sexo masculino. 
 
-
+SELECT AVG(indiceRendimento) FROM aluno WHERE sexo = 'M';
 
 --23) Mostrar a maior nota do índice de rendimento considerando todos os alunos. 
 
+SELECT MAX(indiceRendimento) FROM aluno;
 
+-- Mostrando o nome junto, com subconsulta
+
+SELECT Nomaluno, indiceRendimento
+FROM aluno
+WHERE indiceRendimento = (SELECT MAX(indiceRendimento) FROM aluno);
 
 --24) Mostrar a menor nota do índice de rendimento considerando todos os alunos. 
 
+SELECT MIN(indiceRendimento) FROM aluno;
 
+SELECT Nomaluno, indiceRendimento
+FROM aluno
+WHERE indiceRendimento = (SELECT MIN(indiceRendimento) FROM aluno);
 
 --25) Mostrar a maior e a menor nota do índice de rendimento dos alunos do sexo masculino. 
 
+SELECT MAX(indiceRendimento), MIN(indiceRendimento) FROM aluno WHERE sexo = 'M';
 
+SELECT Nomaluno, indiceRendimento
+FROM aluno
+WHERE sexo = 'M'
+AND (
+	indiceRendimento = (SELECT MAX(indiceRendimento) FROM aluno WHERE sexo = 'M')
+	or
+	indiceRendimento = (SELECT MIN(indiceRendimento) FROM aluno WHERE sexo = 'M')
+);
 
 --26) Mostrar a maior e menor nota do índice de rendimento dos alunos do sexo feminino. 
 
+SELECT MAX(indiceRendimento), MIN(indiceRendimento)
+FROM aluno
+WHERE sexo = 'F';
 
+SELECT Nomaluno, indiceRendimento
+FROM aluno
+WHERE sexo = 'F'
+AND (
+    indiceRendimento = (SELECT MAX(indiceRendimento) FROM aluno WHERE sexo = 'F')
+    OR
+    indiceRendimento = (SELECT MIN(indiceRendimento) FROM aluno WHERE sexo = 'F')
+);
 
 --27) Mostrar o nome do aluno com maior índice de rendimento.  
 
-
+SELECT Nomaluno
+FROM aluno
+WHERE indiceRendimento = (SELECT MAX(indiceRendimento) FROM aluno);
 
 --28) Mostrar o nome do aluno com menor índice de rendimento. 
 
-
+SELECT Nomaluno
+FROM aluno
+WHERE indiceRendimento = (SELECT MIN(indiceRendimento) FROM aluno);
 
 --29) Mostrar os nomes dos alunos que tenham o menor e o maior índice de rendimento, respectivamente. 
 
-
+SELECT Nomaluno
+FROM aluno
+WHERE indiceRendimento = (SELECT MIN(indiceRendimento) FROM aluno)
+	OR indiceRendimento = (SELECT MAX(indiceRendimento) FROM aluno);
 
 --30) Mostrar os nomes dos alunos juntamente com o menor e o maior índice de rendimento, respectivamente. 
 
+SELECT
+Nomaluno,
+indiceRendimento,
+(SELECT MIN(indiceRendimento) FROM aluno) AS MenorIndice,
+(SELECT MAX(indiceRendimento) FROM aluno) AS MaiorIndice
+FROM aluno;
