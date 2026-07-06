93ebf70b12b099d290e17208342dfcc4eefaa0ef
felipedonato280 <felipedonato280@gmail.com>
2026-07-05 22:26:11 -0300

    Finalizando trabalho de POO sobre imposto de renda

--- POO/FolhaDePagamento/src/Funcionario.java
+++ POO/FolhaDePagamento/src/Funcionario.java
@@ -1,82 +1,49 @@
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Funcionario{
-    private String nome;
-    private String cpf;
+public class Funcionario extends Pessoa{
     private float salario;
     private String cargo;
+    private List<Filho> filhos;
 
     // CONSTRUCTOR
 
     public Funcionario(String nome, String cpf, float salario){
-        setNome(nome);
-
-        if (CpfCnpjUtils.isValidCPF(cpf)){
-            this.cpf = cpf;
-        } else {
-            System.out.printf("O CPF %s é inválido. Verifique a informação \n", cpf);
-            this.cpf = "";
-        }
-
+        super(nome, cpf);
         setSalario(salario);
+        this.filhos = new ArrayList<>();
     }
 
     // CONSTRUCTOR OVERLOAD
 
     public Funcionario(String nome, String cpf, float salario, String cargo){
         this(nome, cpf, salario);
-
         setCargo(cargo);
     }
 
     // METODOS PRINCIPAIS
 
-    public void lerFuncionario(){
-        Scanner input = new Scanner(System.in);
-
-        System.out.println("Digite o seu nome: ");
-        nome = input.nextLine();
-
-        System.out.println("Digite o seu CPF: ");
-        cpf = input.nextLine();
-
-        System.out.println("Digite o seu salario: ");
-        salario = input.nextFloat();
-
-        input.nextLine();
-
-        System.out.println("Digite o seu cargo: ");
-        cargo = input.nextLine();
+    public void adicionarFilho(Filho filho){
+        if (filho != null){
+            filhos.add(filho);
+        }
     }
 
+    @Override
     public String toString(){
-        String saidaM1 = "";
-        saidaM1 += "Nome: " + nome + "\n";
-        saidaM1 += "CPF: " + cpf + "\n";
-        saidaM1 += "Salario: " + salario + "\n";
-        saidaM1 += "Cargo: " + cargo + "\n";
+        String saida = super.toString();
 
-        String saidaM2 = "";
-        saidaM2 += "Nome: " + nome + "\n";
-        saidaM2 += "CPF: " + cpf + "\n";
-        saidaM2 += "Salario: " + salario + "\n";
+        saida += "Salario: " + salario + "\n";
 
-        if (cargo == null)
-            return saidaM2;
+        if (cargo != null)
+            saida += "Cargo: " + cargo + "\n";
 
-        return saidaM1;
+        saida += "Dependentes: " + getNumeroDependentes() + "\n";
+
+        return saida;
     }
 
     // SETTERS
-
-    public void setNome(String nome){
-        if(nome != null && !nome.isEmpty()){
-            this.nome = nome;
-        }
-        else{
-            this.nome = "Nome não informado";
-        }
-    }
 
     public void setSalario(float salario){
         if(salario >= 1621){
@@ -99,14 +66,6 @@ public class Funcionario{
 
     // GETTERS
 
-    public String getNome(){
-        return nome;
-    }
-
-    public String getCPF(){
-        return cpf;
-    }
-
     public float getSalario(){
         return salario;
     }
@@ -115,13 +74,7 @@ public class Funcionario{
         return cargo;
     }
 
-    public static void main(String[] args){
-        Funcionario Func1 = new Funcionario("Felipe", "54669825411", 1820.50f, "TI");
-        Funcionario Func2 = new Funcionario("Rafaela", "89654721568", 2520.50f);
-        Funcionario Func3 = new Funcionario("Joao", "45871325846", 5500.99f, "Atendente");
-
-        System.out.println(Func1.toString());
-        System.out.println(Func2.toString());
-        System.out.println(Func3.toString());
+    public int getNumeroDependentes(){
+        return filhos.size();
     }
 }
